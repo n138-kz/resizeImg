7eec2c80db93b441aba4816aac0aa25450fbe6db
Yuu Komiya <8064928+n138-kz@users.noreply.github.com>
2023-04-05 00:59:12 +0900

    Update index.php

--- index.php
+++ index.php
@@ -72,7 +72,34 @@ if ( !isset($_FILES['image']) ) {
 if ( !isset($_FILES['image']['error']) || $_FILES['image']['error'] != 0 ) {
 	http_response_code(500);
 	$exitStatus->setVal('time', time());
-	$exitStatus->setVal('text', 'File error.');
+	switch ($_FILES['image']['error']) {
+		case UPLOAD_ERR_OK:
+			break;
+		case UPLOAD_ERR_INI_SIZE:
+			$exitStatus->setVal('text', 'UPLOAD_ERR_INI_SIZE');
+			break;
+		case UPLOAD_ERR_FORM_SIZE:
+			$exitStatus->setVal('text', 'UPLOAD_ERR_FORM_SIZE');
+			break;
+		case UPLOAD_ERR_PARTIAL:
+			$exitStatus->setVal('text', 'UPLOAD_ERR_PARTIAL');
+			break;
+		case UPLOAD_ERR_NO_FILE:
+			$exitStatus->setVal('text', 'UPLOAD_ERR_NO_FILE');
+			break;
+		case UPLOAD_ERR_NO_TMP_DIR:
+			$exitStatus->setVal('text', 'UPLOAD_ERR_NO_TMP_DIR');
+			break;
+		case UPLOAD_ERR_CANT_WRITE:
+			$exitStatus->setVal('text', 'UPLOAD_ERR_CANT_WRITE');
+			break;
+		case UPLOAD_ERR_EXTENSION:
+			$exitStatus->setVal('text', 'UPLOAD_ERR_EXTENSION');
+			break;
+		default:
+			$exitStatus->setVal('text', 'File error.');
+			break;
+	}
 	$exitStatus->setVal('text', $exitStatus->getVal('text') . ' ');
 	$exitStatus->setVal('text', $exitStatus->getVal('text') . 'Error-code: ' . $_FILES['image']['error']);
 	if ( (bool)($_SERVER['HTTP_X_SCRIPT_DEBUG']) ) {
